1402a8c3e70c7419de9e0cf670d8903bb520a76b
Шекунов Андрей <asheknov@gmail.com>
2022-09-12 19:54:09 +0500

    Задача 26 определение кол-ва цифр в числе с помощью метода

---
+++ task26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число
+// и выдаёт количество цифр в числе.
+// Например:
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+int numberOfDigital(int n) // метод определения кол-ва цифр в числе
+{
+    int i = 0;
+    for (i = 0; n > 0; i++)
+    {
+        n = n / 10;
+    }
+    return i;
+}
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digital = numberOfDigital(number);
+Console.WriteLine($"Количество цифр в числе {number} = {digital}");
